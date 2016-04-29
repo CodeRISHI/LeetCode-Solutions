dc33f0be8d6e25348b375649240fbaa2b36b1608
Arun Vasudevan <arunv83@gmail.com>
2016-04-28 22:42:51 -0700

    Complete moveZeroes.js

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # LeetCode-Solutions
 ### LeetCode problems for practice
 
-[x] singleNumber
-[x] addDigits
-[x] maxDepth of Binary Tree
-[x] invert Binary Tree
+    [x] singleNumber
+    [x] addDigits
+    [x] maxDepth of Binary Tree
+    [x] invert Binary Tree
+    [x] move Zeroes
---
+++ moveZeroes.js
@@ -0,0 +1,14 @@
+
+var moveZeroes = function(nums) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      nums.push(JSON.parse(nums.splice(i, 1).join('')));
+    }
+  }
+  return nums;
+};
+
+//TEST
+var nums = [0, 1, 0, 3, 12];
+
+console.log(moveZeroes(nums)); //[1, 3, 12, 0, 0]
